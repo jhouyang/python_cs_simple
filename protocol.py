0dd6c68fb7c4eaf6cbb4774825816ba8f77554f0
jouyang <oyjh1986@qq.com>
2014-02-10 23:25:24 +0800

    fix a bug, add handle connection function declaration

--- protocol.py
+++ protocol.py
@@ -15,22 +15,37 @@ class ProtoCol(object):
     ERROR, INFO, RESP, CLOSE = ('err ', 'inf ', 'res ', 'clo ')
     @classmethod
     def send_response(sock, msg):
-        send_msg = struct.pack('>4si%ds', ProtoCol.RESP, len(msg), msg)
+        send_msg = struct.pack('>4si%ds'%len(msg), ProtoCol.RESP, msg)
         sock.sendall(send_msg)
 
     @classmethod
     def send_info(sock, msg):
-        send_msg = struct.pack('>4si%ds', ProtoCol.INFO, len(msg), msg)
+        send_msg = struct.pack('>4si%ds'%len(msg), ProtoCol.INFO, msg)
         sock.sendall(send_msg)
 
     @classmethod
     def send_error(sock, msg):
-        send_msg = struct.pack('>4si%ds', ProtoCol.ERROR, len(msg), msg)
+        send_msg = struct.pack('>4si%ds'%len(msg), ProtoCol.ERROR, msg)
         sock.sendall(send_msg)
 
     @classmethod
     def send_close(sock, msg):
-        send_msg = struct.pack('>4si%ds', ProtoCol.CLOSE, len(msg), msg)
+        send_msg = struct.pack('>4si%ds'%len(msg), ProtoCol.CLOSE, msg)
         sock.sendall(send_msg)
+
+
+def handle_conn(sock):
+    '''deserialize data,
+    return tuple of type, string data
+    '''
+    pass
+
+def read_info(sock):
+    '''read sock data,
+    return (type, data)
+    '''
+    init_size = 1024
+    msg_raw = sock.recv(init_size)
+    msg_str = struct.unpack('>4si%ds', msg_raw)
 
 
