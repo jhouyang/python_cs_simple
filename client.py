366f1b4b1b1fc77a33f46b36a8ed14252325dce6
jhouyang <oyjh1986@qq.com>
2014-02-11 11:01:41 +0800

    add connect function and initial args parser

--- client.py
+++ client.py
@@ -1,1 +1,32 @@
 #!/usr/bin/env python
+
+'''This is the client file
+'''
+
+import socket
+
+def connect(host, port):
+    '''connect to (host, port)
+    '''
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+      sock.connect((host, port))
+    except socket.error, e:
+      print e
+      sys.exit(-1)
+      
+    return sock
+  
+  
+def main():
+    '''main function of client
+    '''
+    arg = sys.argv[1:]
+    if len(arg) != 2:
+        print 'Please input valid host and port number'
+        sys.exit(-1)
+        
+    sock = connect(arg[0], arg[1])
+    
+if __name__ == '__main__':
+    main()
